b642cd7777ad88d228e6b430cb57bc37e8a7cc6d
Abdalrahmaan Mohammad Alsayed <65967579+Abdo-404@users.noreply.github.com>
2026-03-24 11:40:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ document.querySelectorAll(".fade").forEach(el => observer.observe(el));
 function toggleMenu(){
   document.getElementById("menu").classList.toggle("show");
 }
+
+
+function includeHTML() {
+  const elements = document.querySelectorAll("[data-include]");
+
+  elements.forEach(el => {
+    const file = el.getAttribute("data-include");
+
+    fetch(file)
+      .then(res => res.text())
+      .then(data => {
+        el.innerHTML = data;
+      });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", includeHTML);
